73222f52ec4287d88fdc3e745835839ad5834835
Trevor Vandehey <trevorvandehey@gmail.com>
2015-02-26 23:43:50 +0000

    created csv uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224044710) do
+ActiveRecord::Schema.define(version: 20150226004433) do
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.string   "manager"
+    t.string   "status"
+    t.integer  "terms"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "invoices", force: true do |t|
     t.datetime "date"
